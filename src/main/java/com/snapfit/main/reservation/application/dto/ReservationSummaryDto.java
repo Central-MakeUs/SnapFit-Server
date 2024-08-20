ce2c2212aad50675f380a8991a5f9ccf6a9c4d3f
Yongha Kwon <yongha.kwon12@gmail.com>
2024-08-20 21:42:27 +0900

    [fix] 문서화 진행중

--- src/main/java/com/snapfit/main/reservation/application/dto/ReservationSummaryDto.java
+++ src/main/java/com/snapfit/main/reservation/application/dto/ReservationSummaryDto.java
@@ -1,6 +1,7 @@
 package com.snapfit.main.reservation.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,8 +12,10 @@ import java.time.LocalDateTime;
 public class ReservationSummaryDto {
     private long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Schema(example  = "2024-12-11 12:00:00")
     private LocalDateTime reservationTime;
     private PostSummaryDto post;
     private Integer totalPrice;
+    @Schema(description = "null 인 경우 취소되지 않은 데이터")
     private String cancelMessage;
 }
